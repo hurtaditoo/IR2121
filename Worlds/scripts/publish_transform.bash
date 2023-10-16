7e84a4085c6fa09fa8aea98f25c23c656a257618
irene <al415507@uji.es>
2023-10-16 19:36:46 +0200

    fisico

--- Worlds/scripts/publish_transform.bash
+++ Worlds/scripts/publish_transform.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /opt/ros/foxy/setup.bash
-export ROS_LOCALHOST_ONLY=1
-ros2 run tf2_ros static_transform_publisher 1.15 8.85 0 0 0 0 1 map odom
+ros2 run tf2_ros static_transform_publisher -5.2 3 0 0 0 0.1564345 0.9876883 map odom
--- Worlds/scripts/visualize_localization.bash
+++ Worlds/scripts/visualize_localization.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /opt/ros/foxy/setup.bash
 export ROS_LOCALHOST_ONLY=1
-ros2 bag record /map /odom /scan /tf /tf_static
+rviz2 -d config_amcl.rviz
+
