75e25877d746fe6397751cc0f78f61624b507a55
hurtaditoo <al415507@uji.es>
2023-10-23 19:17:26 +0200

    cambios

--- Worlds/scripts/publish_transform.bash
+++ Worlds/scripts/publish_transform.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/ros/foxy/setup.bash
-ros2 run tf2_ros static_transform_publisher -5.2 3 0 0 0 0.1564345 0.9876883 map odom
+ros2 run tf2_ros static_transform_publisher 12.2 -30 0 0 0 0.1305262 0.991449 map odom
