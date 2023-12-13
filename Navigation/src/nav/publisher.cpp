d9b7a10c9831645111cda8c6088fa097d809fd0f
hurtaditoo <al415507@uji.es>
2023-12-13 17:22:46 +0100

    Version 4 lista

--- Navigation/src/nav/publisher.cpp
+++ Navigation/src/nav/publisher.cpp
@@ -1,17 +1,17 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include <iostream>
 #include <cstdlib>
 
 using namespace std::chrono_literals;
 
-double lodom, wodom;
-void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+double AMCLx, AMCLy;
+void topic_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
 {
-   lodom = msg->twist.twist.linear.x;
-   wodom = msg->twist.twist.angular.z;
+   AMCLx = msg->pose.pose.position.x;
+   AMCLy = msg->pose.pose.position.y;
 }
 
 int main(int argc, char * argv[])
@@ -19,20 +19,24 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
-  auto subscription = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback);
+  auto subscription = node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("amcl_pose", 10, topic_callback);
   geometry_msgs::msg::PoseStamped message;
   
   rclcpp::WallRate loop_rate(500ms);
     
-  double x[4]={-1, 4, 8.08, 2.9}, y[4]={3, 4.85, 0.12, 0.91};
+  double x[4]={-1, 4, 8.08, 2.9}, y[4]={3, 4.85, 0.12, 0.91}, difx, dify;
   int contador = 0;	// Contador para saber por cual posición voy
+  
   while (rclcpp::ok() && contador <= 4) {
     message.pose.position.x = x[contador];
     message.pose.position.y = y[contador];
     
     publisher->publish(message);
     
-    if ((std::abs(lodom) < 0.05) && (std::abs(wodom) < 0.05))
+    difx = std::abs(x[contador] - AMCLx); 
+    dify = std::abs(y[contador] - AMCLy); 
+    
+    if ((difx < 0.3) && (dify < 0.3))
     	contador++;
     
     rclcpp::spin_some(node);
