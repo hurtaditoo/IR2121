3e5a3462919b522f42af81f0acc18b05d86c78fd
hurtaditoo <al415507@uji.es>
2023-12-13 17:36:14 +0100

    probar version 4 fisico

---
+++ Navigation/src/nav_fisico/publisher.cpp
@@ -0,0 +1,49 @@
+#include <chrono>
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std::chrono_literals;
+
+double AMCLx, AMCLy;
+void topic_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
+{
+   AMCLx = msg->pose.pose.position.x;
+   AMCLy = msg->pose.pose.position.y;
+}
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("publisher");
+  auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
+  auto subscription = node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("amcl_pose", 10, topic_callback);
+  geometry_msgs::msg::PoseStamped message;
+  
+  rclcpp::WallRate loop_rate(500ms);
+    
+  double x[4]={-1, 4, 8.08, 2.9}, y[4]={3, 4.85, 0.12, 0.91}, difx, dify;
+  int contador = 0;	// Contador para saber por cual posición voy
+  
+  while (rclcpp::ok() && contador <= 4) {
+    message.pose.position.x = x[contador];
+    message.pose.position.y = y[contador];
+    
+    publisher->publish(message);
+    
+    difx = std::abs(x[contador] - AMCLx); 
+    dify = std::abs(y[contador] - AMCLy); 
+    
+    if ((difx < 0.3) && (dify < 0.3))
+    	contador++;
+    
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+
+  }
+  rclcpp::shutdown();
+  return 0;
+}
+
