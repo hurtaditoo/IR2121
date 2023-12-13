ae4c117f0b2e93791e4399054a3ab29aef76cf3f
hurtaditoo <al415507@uji.es>
2023-12-13 21:22:37 +0100

    ni se que hago ya

--- Navigation/src/nav/publisher.cpp
+++ Navigation/src/nav/publisher.cpp
@@ -2,25 +2,24 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "tf2_ros/transform_listener.h"
-#include "geometry_msgs/TransformStamped.h"
+#include "geometry_msgs/transform_stamped.hpp"
 #include <iostream>
 #include <cstdlib>
 
 using namespace std::chrono_literals;
-
-double AMCLx, AMCLy;
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
-  
-  tf2_ros::Buffer tfBuffer;
-  tf2_ros::TransformListener tfListener(tfBuffer);
-  
+    
+  geometry_msgs::TransformStamped transformStamped; 
   geometry_msgs::msg::PoseStamped message;
   
+  <tf2_ros/Buffer>tf2_ros::Buffer buffer;
+  <tf2_ros/TransformListener>tf2_ros::TransformListener listener(buffer);
+
   rclcpp::WallRate loop_rate(500ms);
     
   double x[4]={-1, 4, 8.08, 2.9}, y[4]={3, 4.85, 0.12, 0.91}, difx, dify;
@@ -32,11 +31,13 @@ int main(int argc, char * argv[])
     
     publisher->publish(message);
     
-    difx = std::abs(x[contador] - AMCLx); 
-    dify = std::abs(y[contador] - AMCLy); 
+    difx = std::abs(message.pose.position.x); 
+    dify = std::abs(message.pose.position.y); 
     
     if ((difx < 0.3) && (dify < 0.3))
-    	contador++;
+    	contador++;    
+    	
+    transformStamped = buffer.lookupTransform("base_link", "map" rrclp::Time(0), rrclp::Duration(5);
     
     rclcpp::spin_some(node);
     loop_rate.sleep();
