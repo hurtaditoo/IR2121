c28504faf422aead93ced6219439d68818bdd2b8
hurtaditoo <al415507@uji.es>
2023-12-18 18:59:01 +0100

    version 5 fisico final

--- Navigation/build/nav/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/usuario/Documentos/GitHub/IR2121/Navigation/src/nav
-# Build directory: /home/usuario/Documentos/GitHub/IR2121/Navigation/build/nav
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Navigation/src/nav_fisico/publisher.cpp
+++ Navigation/src/nav_fisico/publisher.cpp
@@ -1,27 +1,27 @@
 #include <chrono>
-#include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include <iostream>
 #include <cstdlib>
 
-using namespace std::chrono_literals;
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
-double AMCLx, AMCLy;
-void topic_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
-{
-   AMCLx = msg->pose.pose.position.x;
-   AMCLy = msg->pose.pose.position.y;
-}
+using namespace std::chrono_literals;
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
-  auto subscription = node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("amcl_pose", 10, topic_callback);
-  geometry_msgs::msg::PoseStamped message;
+    
+  geometry_msgs::msg::TransformStamped transformStamped; 
+  geometry_msgs::msg::PoseStamped message, actual;
   
+  std::unique_ptr<tf2_ros::Buffer> buffer = std::make_unique<tf2_ros::Buffer>(node->get_clock());
+  std::shared_ptr<tf2_ros::TransformListener> listener = std::make_shared<tf2_ros::TransformListener>(*buffer);	// No lo dejo como listener(buffer) porque cambio el std::make_unique y make_shared
+
   rclcpp::WallRate loop_rate(500ms);
     
   double x[3]={2.8, -3.21, -9.5}, y[3]={-6.1, -4.1, -4.5}, difx, dify;
@@ -33,11 +33,14 @@ int main(int argc, char * argv[])
     
     publisher->publish(message);
     
-    difx = std::abs(x[contador] - AMCLx); 
-    dify = std::abs(y[contador] - AMCLy); 
+    transformStamped = buffer->lookupTransform("base_footprint", "map", rclcpp::Time(0), rclcpp::Duration(5s));   
+    tf2::doTransform(message, actual, transformStamped);
+    
+    difx = std::abs(actual.pose.position.x); 
+    dify = std::abs(actual.pose.position.y); 
     
     if ((difx < 0.3) && (dify < 0.3))
-    	contador++;
+    	contador++;    
     
     rclcpp::spin_some(node);
     loop_rate.sleep();
