6a5d28d0abace019aa687344bce639150efd6e3e
hurtaditoo <al415507@uji.es>
2023-12-18 18:35:43 +0100

    version 5 ya esta!

---
+++ Navigation/build/nav_fisico/CMakeFiles/nav_fisico_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nav_fisico_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/nav_fisico_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Navigation/src/nav/publisher.cpp
+++ Navigation/src/nav/publisher.cpp
@@ -5,7 +5,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "tf2_ros/transform_listener.h"
-#include "geometry_msgs/transform_stamped.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 using namespace std::chrono_literals;
 
@@ -15,10 +16,10 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
     
-  geometry_msgs::TransformStamped transformStamped; 
-  geometry_msgs::msg::PoseStamped message;
+  geometry_msgs::msg::TransformStamped transformStamped; 
+  geometry_msgs::msg::PoseStamped message, actual;
   
-  std::unique_ptr<tf2_ros::Buffer> buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  std::unique_ptr<tf2_ros::Buffer> buffer = std::make_unique<tf2_ros::Buffer>(node->get_clock());
   std::shared_ptr<tf2_ros::TransformListener> listener = std::make_shared<tf2_ros::TransformListener>(*buffer);	// No lo dejo como listener(buffer) porque cambio el std::make_unique y make_shared
 
   rclcpp::WallRate loop_rate(500ms);
@@ -32,11 +33,11 @@ int main(int argc, char * argv[])
     
     publisher->publish(message);
     
-    difx = std::abs(message.pose.position.x); 
-    dify = std::abs(message.pose.position.y); 
+    transformStamped = buffer->lookupTransform("base_footprint", "map", rclcpp::Time(0), rclcpp::Duration(5s));   
+    tf2::doTransform(message, actual, transformStamped);
     
-    transformStamped = buffer->lookupTransform("base_footprint", "map", rrclp::Time(0), rrclp::Duration(5s);   
-    tf2::doTransform(message, transformStamped);
+    difx = std::abs(actual.pose.position.x); 
+    dify = std::abs(actual.pose.position.y); 
     
     if ((difx < 0.3) && (dify < 0.3))
     	contador++;    
